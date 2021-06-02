df33631acb60ec0e6ef7c537622120ce18e01b24
kafleprabhakar <pkafleprabhakar@gmail.com>
2021-06-02 01:42:03 -0400

    final style

--- src/App.js
+++ src/App.js
@@ -3,73 +3,76 @@ import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 // Import stylesheets
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './style.scss';
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
 
       {/* Landing section  */}
-      <Container className="Container Homepage-bg" fluid>
-        <h1>COVID RESOURCES NEPAL</h1>
-        <p>
-          A one liner here.
-        </p>
-        <br />
+      <Container className="section Homepage-bg">
+
+        <div className="top-hero">
+          <h1 id="hero-text">COVID RESOURCES NEPAL</h1>
+          <p id="hero-subtitle">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+        </div>
 
         <Row>
           <Col>
-            <Button variant="danger" size="lg">Donations</Button> 
+            <Button variant="primary" size="lg">Donations</Button> 
           </Col>
           <Col>
-            <Button variant="danger" size="lg">Resources</Button>
+            <Button variant="primary" size="lg">Resources</Button>
           </Col>
+          <Col>
+            <Button variant="primary" size="lg">Vaccine Petition</Button>
+          </Col>
+        </Row>
+        <Row>
         </Row>
       </Container>
 
       {/* Help section  */}
-      <Container className="Container Homepage-help" fluid> 
-        <h1>How can you help?</h1>
-        <br />
+      <Container className="section Homepage-help" fluid>
+        <h1 className="section-header">How you can help</h1>
 
-        <Row>
-          <Col>
-            <Container className="Card-container">
-              <h2>Donate</h2>
+        <Row className="justify-content-around align-items-center">
+          <Col sm={12} md={6} lg={5}>
+            <Card body className="help-card">
+              <h3>Donate</h3>
               <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
               eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Enim neque volutpat ac tincidunt vitae semper quis. 
               Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. 
-              Facilisis gravida neque convallis a cras. Lorem ipsum dolor sit amet. 
-              Tristique senectus et netus et malesuada fames.
               </p>
-            </Container>
+              <Button variant="outline-primary">Donate</Button>
+            </Card>
           </Col>
-          <Col>
-            <Container className="Card-container">
-              <h2>More Resources</h2>
-              <div className="Card-body">
+          <Col sm={12} md={6} lg={5}>
+            <Card body className="help-card">
+              <h3>Add More Resources</h3>
+              <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do 
               eiusmod tempor incididunt ut labore et dolore magna aliqua. 
               Enim neque volutpat ac tincidunt vitae semper quis. 
-              Nunc congue nisi vitae suscipit tellus mauris a diam maecenas. 
-              Facilisis gravida neque convallis a cras. Lorem ipsum dolor sit amet. 
-              Tristique senectus et netus et malesuada fames.
-              </div>
-            </Container>
+              Nunc congue nisi vitae suscipit tellus mauris a diam maecenas.
+              </p>
+              <Button variant="outline-primary">Add Resource</Button>
+            </Card>
           </Col> 
         </Row>
       </Container>
 
       {/* About Us Section */}
-      <Container className="Container Homepage-about" fluid>
-        <h1>About us</h1>
-        <br />
-        <br />
+      <Container className="section Homepage-about">
+        <h1 className="section-header">About us</h1>
 
         <div className="Homepage-about-paragraph">
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
@@ -81,22 +84,22 @@ function App() {
         leo in vitae. Convallis aenean et tortor at risus. Imperdiet nulla
         malesuada pellentesque elit. Mollis nunc sed id semper risus in
         hendrerit gravida rutrum. Pretium fusce id velit ut tortor pretium.
-        Ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a. 
-        Tortor posuere ac ut consequat semper viverra nam libero justo.
-        Felis bibendum ut tristique et egestas. Congue nisi vitae suscipit
-        tellus mauris. Integer enim neque volutpat ac tincidunt vitae.
-        Justo donec enim diam vulputate ut pharetra sit amet aliquam.
-        In vitae turpis massa sed elementum. Sed libero enim sed faucibus.
         </div>
       </Container>
 
       {/* Footer  */}
-      <Container className="Footer" fluid>
-        <div className="container_footer">
-          <font size="3">Covid Resources Nepal</font>
-            email@gmail.com
-        </div>
-      </Container>
+      <footer className="Footer">
+        <Container>
+          <Row className="justify-content-between">
+            <Col>
+              &copy; Covid Resources Nepal
+            </Col>
+            <Col>
+              Contact: covidresourcesnepal@gmail.com
+            </Col>
+          </Row>
+        </Container>
+      </footer>
     </div>
   );
 }
