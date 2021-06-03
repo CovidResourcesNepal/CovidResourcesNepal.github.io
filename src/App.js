3fc0c5de91c2b689013c0393c15672bea400d96a
kafleprabhakar <pkafleprabhakar@gmail.com>
2021-06-03 09:47:02 -0400

    final first iteration

--- src/App.js
+++ src/App.js
@@ -41,20 +41,26 @@ export default class App extends React.Component {
         <Container className="section Homepage-bg">
 
           <div className="top-hero">
-            <h1 id="hero-text">Covid Resources for Nepal</h1>
-            <p id="hero-subtitle">A common source for resources, donation campaigns, support systems, and Nepal’s response to Covid-19 in one place</p>
+            <h1 id="hero-text">COVID Resources for Nepal</h1>
+            <p id="hero-subtitle">A common source for resources, donation campaigns, support systems, and Nepal’s response to COVID-19 in one place</p>
           </div>
 
           <Row>
             <Col>
-              <Button variant="primary" size="lg" href="https://www.google.com/" target="_blank">Fundraisers</Button> 
+              <Button variant="primary" size="lg" target="_blank" 
+                href="https://docs.google.com/spreadsheets/d/1BQZvGqM3Ao48A6lHo_U1yfkFzb8DWPPnqhExFd6X8-8/edit#gid=570437755">
+                  Fundraisers
+              </Button>
             </Col>
             <Col>
-              <Button variant="primary" size="lg" href="https://www.google.com/" target="_blank">Resources</Button>
+              <Button variant="primary" size="lg" target="_blank" 
+                href="https://docs.google.com/spreadsheets/d/1b6GsLdzSwY6Y60Z1-sQGegIJbcB_cCaqH6Hn76KI9Fk/edit#gid=0">
+                  Resources
+              </Button>
             </Col>
             <Col>
               <Button variant="primary" size="lg" target="_blank"
-                href="https://www.change.org/p/ambassador-randy-berry-get-covid-vaccines-to-nepal-asap-and-prevent-a-humanitarian-disaster">
+                href="https://www.change.org/p/urge-us-government-to-send-emergency-covid-19-vaccine-humanitarian-aid-to-nepal">
                   Vaccine Petition
               </Button>
             </Col>
@@ -72,35 +78,46 @@ export default class App extends React.Component {
               <Card body className="help-card">
                 <h3>Donate</h3>
                 <p>
-                  We have compiled a list of fundraisers trying to appeal to a larger community and allocate funds
-                  to support Nepal in tackling and minimizing the effects of the Covid-19. These fundraisers are
-                  supporting initiatives on the ground that are related to medical needs, food distribution, delivery
-                  of home isolation kits among many others. We have tried to give a brief overview of each campaign.
-                  Please feel free to scroll through and support these fundraisers to the best of your ability. Every
-                  donation counts, every little effort adds up. 
+                  We have compiled a list of fundraisers trying to appeal to a larger community and allocate funds to 
+                  support Nepal in tackling and minimizing the effects of the Covid-19. These fundraisers are supporting 
+                  initiatives on the ground that are related to medical needs, food distribution, delivery of home isolation 
+                  kits among many others. We have tried to give a brief overview of each campaign. Please feel free to 
+                  scroll through and support these fundraisers to the best of your ability. Every donation counts, every 
+                  little effort adds up. 
                 </p>
-                <Button variant="outline-primary" href="https://www.google.com/" target="_blank">Donate</Button>
+                <Button variant="outline-primary" target="_blank" 
+                  href="https://docs.google.com/spreadsheets/d/1BQZvGqM3Ao48A6lHo_U1yfkFzb8DWPPnqhExFd6X8-8/edit#gid=570437755">
+                    Donate
+                </Button>
               </Card>
             </Col>
             <Col sm={12} md={6} lg={4}>
               <Card body className="help-card">
-                <h3>Add More Resources</h3>
+                <h3>Add to the Database</h3>
                 <p>
-                  Our volunteers have worked day and night to compile the data that we have. However, we believe there
-                  are many more resources and campaigns that need to be listed alongside. If you know of one, whether
-                  if be resources or campaigns or any support systems, please use the form below to let us know.
+                  Our volunteers have worked day and night to compile the data that we have. However, we believe there are 
+                  many more resources, campaigns, and fundraisers that need to be listed alongside. If you know of one, whether 
+                  it be resources, campaigns or any support systems, please use the form below to let us know.
                 </p>
-                <Button variant="outline-primary" href="https://www.google.com/" target="_blank">Add Resource</Button>
+                <Button variant="outline-primary" target="_blank" 
+                  href="https://docs.google.com/forms/d/e/1FAIpQLScbwDZCkG8iiacpJb5rFBH71EC3wUExG_q-D2o_mAUpLlhHGQ/viewform">
+                    Add Resource
+                </Button>
               </Card>
             </Col> 
             <Col sm={12} md={6} lg={4}>
               <Card body className="help-card">
-                <h3>Sign Petition</h3>
+                <h3>Sign Vaccine Petition</h3>
                 <p>
-                  Please sign the petition to urge lawmakers to support vaccine donations to Nepal.
+                  Only about 4% of the population has received at least one dose of the Covid-19 vaccine and it is unlikely 
+                  Nepal will receive more vaccines from its primary source, India, in the near future provided the crisis India 
+                  itself is going through. So, countries like the US could really help fight this pandemic in Nepal by donating 
+                  vaccines to the Government of Nepal. This will forestall immeasurable devastation for the people of Nepal. 
+                  Please sign this petition organized by Nepal Rising to urge the US Department of State to support vaccine 
+                  donations to Nepal. 
                 </p>
                 <Button variant="outline-primary" target="_blank"
-                  href="https://www.change.org/p/ambassador-randy-berry-get-covid-vaccines-to-nepal-asap-and-prevent-a-humanitarian-disaster">
+                  href="https://www.change.org/p/urge-us-government-to-send-emergency-covid-19-vaccine-humanitarian-aid-to-nepal">
                     Sign Petition
                 </Button>
               </Card>
@@ -109,18 +126,18 @@ export default class App extends React.Component {
         </Container>
 
         {/* About Us Section */}
-        <Container className="section Homepage-about">
+        <Container className="section Homepage-about pb-5">
           <h1 className="section-header">About us</h1>
 
           <div className="Homepage-about-paragraph">
-            We are a group of Nepali students and young professionals trying to integrate all the resources dedicated to
-            Nepal’s response to Covid-19 in one place. We hope that this platform can make the search for various types
-            of resources and impactful fundraisers effective, including those people/organizations that need to address
-            emergency situations in a short amount of time. 
+            We are a group of Nepali students and young professionals trying to integrate all the resources dedicated to Nepal’s
+            response to Covid-19 in one place. We hope that this platform can make the search for various types of resources and 
+            impactful fundraisers effective, including those people/organizations that need to address emergency situations in 
+            a short amount of time. 
           </div>
         </Container>
 
-        <Container className="section homepage-team pt-0">
+        <Container className="section homepage-team pt-0 pb-5">
           <h3 className="section-header">Core Team</h3>
           <TeamGallery members={this.state.team.core}/>
         </Container>
@@ -131,14 +148,14 @@ export default class App extends React.Component {
         </Container>
 
         {/* Footer  */}
-        <footer className="Footer">
+        <footer className="footer">
           <Container>
             <Row className="justify-content-between">
-              <Col>
+              <Col xs={12} md="auto">
                 &copy; Covid Resources Nepal
               </Col>
-              <Col>
-                Contact: covidresourcesnepal@gmail.com
+              <Col xs={12} md="auto">
+                Contact: <a href="mailto:covidresourcesnepal@gmail.com" target="_blank" rel="noreferrer">covidresourcesnepal@gmail.com</a>
               </Col>
             </Row>
           </Container>
--- src/components/TeamGallery.js
+++ src/components/TeamGallery.js
@@ -1,17 +1,23 @@
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Card from 'react-bootstrap/Card'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
 function TeamGallery(props) {
 
     console.log(props);
+
+    var linkedin = (url) =>
+        <a href={url} target="_blank" rel="noreferrer"><FontAwesomeIcon icon={faLinkedin} /></a>
     
     const teamMembers = props.members.map((member, idx) => 
-        <Col xs={12} md={5} key={idx}>
+        <Col xs={6} md={4} key={idx}>
             <Card className="team-card">
                 {member.name}
                 <br/>
                 {(member.hasOwnProperty("email")) ? member.email : ''}
+                {member.hasOwnProperty("linkedin") ? linkedin(member.linkedin) : ''}
             </Card>
         </Col>
     );
