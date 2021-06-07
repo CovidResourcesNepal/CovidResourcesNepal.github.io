d13bcdfd7cbb14f18f883427df1aed0234d5cd08
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-06-07 14:41:59 -0400

    Added tabs and a placeholder sheet

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Tabs from './components/Tabs/tabs';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Tabs />
   </React.StrictMode>,
   document.getElementById('root')
 );
