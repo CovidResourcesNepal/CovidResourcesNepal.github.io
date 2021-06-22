3050bf056564287c5da8ba658bd271fc780e5901
gyawalisaurav <saurav.gyawali@yahoo.com>
2021-06-22 14:34:58 -0400

    Added translation framework and translation for homepage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,13 @@
 import {
   Container,
   Nav,
-  Navbar
+  Navbar,
+  ToggleButtonGroup,
+  ToggleButton
 } from 'react-bootstrap'
 import { Link } from "react-router-dom";
 
-function Header() {
+function Header({changeLng, lng}) {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" id="site-navbar" fixed="top">
       <Container>
@@ -15,6 +17,15 @@ function Header() {
           <Nav className="justify-content-end">
             <Nav.Link as={Link} to="/">Home</Nav.Link>
             <Nav.Link as={Link} to="/about">About Us</Nav.Link>
+            <ToggleButtonGroup type="radio" defaultValue={lng=="en" ? 1 : 2} name="options" className="language-tog">
+              <ToggleButton value={1} id="en" variant="outline-secondary" onClick={() => changeLng("en")}>
+                en
+              </ToggleButton>
+              <ToggleButton value={2} id="np" variant="outline-secondary" onClick={() => changeLng("np")}>
+                np
+              </ToggleButton>
+            </ToggleButtonGroup>
+
           </Nav>
         </Navbar.Collapse>
       </Container>
